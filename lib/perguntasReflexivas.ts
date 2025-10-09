3d1138e54b4b8a5c7346928f60f21beb21f2833a
simonebrusco <simonebrusco@gmail.com>
2025-10-09 18:14:41 -0300

    Revert "Builder.io: Update from Visual Editor (#129)" (#142)
    This reverts commit 14ab437a50a4ed554f38ab7d898492d2d47ee370.

--- lib/perguntasReflexivas.ts
+++
@@ -1,9 +0,0 @@
-export const perguntasReflexivas = [
-  "O que você fez por você hoje?",
-  "Qual momento te fez sorrir hoje?",
-  "Qual pequeno passo pode deixar seu dia mais leve?",
-  "Você tem se permitido descansar?",
-  "Por que você tem orgulho de si hoje?",
-  "Qual cuidado rápido você pode fazer agora?",
-  "O que você pode simplificar hoje?"
-];
